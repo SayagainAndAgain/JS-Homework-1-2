e1b2aa1c09a38750ae8b2def4760e28df5f60633
Oleksandr Zazymko <ukff@ukr.net>
2016-10-03 14:21:21 +0300

    added check for null, empty, digits and integer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 function pow(x, n){
+	// check for null, empty, digits and integer
+	if (x==null || n==null || x=='' || n=='' || x.replace (/\d/g, '').length || n.replace (/\d/g, '').length || ((x ^ 0) === x) || ((n ^ 0) === n) ) return alert ('Sorry. Wrong x or n.')
+
+
 	var result=1;
 	if(n==0){
 		alert(x+' ^ '+n+' = '+'1');
@@ -23,7 +27,13 @@ var users=[];
 var tempWords=['первое', 'второе', 'третье', 'четвертое', 'пятое'];
 
 for (var i=0; i<5; i++){
-	users.push(prompt('Необходимо добавить ного пользователя. Введите ' + tempWords[i] + ' имя: ', ''));
+	var newUser = prompt('Необходимо добавить нового пользователя. Введите ' + tempWords[i] + ' имя: ', '');
+	if (newUser == null || newUser == ''){
+		i -=1;
+	}
+	else{
+		users.push(newUser);
+	}
 }
 
 var loginUser = prompt('Введите имя пользователя:', '');
@@ -38,5 +48,5 @@ if (coincidence==true){
 	alert (loginUser + ', вы успешно вошли.');
 }
 else{
-	alert ('Упс, неверное имя ползователя.');
+	alert ('Упс, неверное имя пользователя.');
 }
